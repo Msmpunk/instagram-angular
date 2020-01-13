e56e8da47c9953fe971dbb91169de28fcbb87319
Msmpunk <msm0102712@gmail.com>
2020-01-13 12:57:57 -0600

    + Agregamos router de componentes y el componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppLogin } from './login/login.component';
+import { AppHome } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLogin,
+    AppHome
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
